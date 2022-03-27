a5b475d27ca5deddde3ba438f4c1915584570aa5
Chris_Stone <ircstones@gmail.com>
2022-03-27 10:45:10 -0700

    Installed modules and updated tvars file

--- first_code.tf
+++ first_code.tf
@@ -7,4 +7,11 @@ resource "aws_s3_bucket" "tf_course" {
   bucket = "tf-course-20210327" 
   acl    = "private"
 }
-
+# Varibale example
+variable "my-var" {
+  
+  description = "This is a varibale example"
+  type = string
+  default = "Hello"
+  sensitive  = false
+}
